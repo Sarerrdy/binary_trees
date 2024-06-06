edfeadbea0dfa6cc11d003bd90824a4893d75a22
Edmond Ina <sarerrdy4live@gmail.com>
2024-06-06 14:55:59 -0700

    updated 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -4,8 +4,7 @@
  * bst_remove - removes a node from a Binary Search Tree
  * @root: a pointer to the root node of the tree where you will remove a node
  * @value: the value to remove in the tree
- * Return: a pointer to the new root node of the tree after removal
- *         NULL on failure
+ * Return: a pointer to the new root node of the tree
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
