5cd9aab8acf9a5a24222405a659866aae89e59c3
Edmond Ina <sarerrdy4live@gmail.com>
2024-06-06 14:26:32 -0700

    updated 111-bst_insert.c

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,37 +1,46 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
+ * bst_insert - Inserts a value in a Binary Search Tree.
+ * @tree: Double pointer to the root node of the BST.
+ * @value: Value to store in the new node.
  *
- * Return: 1 if tree is a valid BST
- *         0 otherwise
+ * Return: Pointer to the created node, or NULL on failure.
  */
-int binary_tree_is_bst(const binary_tree_t *tree)
+bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (!tree)
-		return (0);
-	return (btib_helper(tree, INT_MIN, INT_MAX));
-}
+	bst_t *current, *new_node;
 
-/**
- * btib_helper - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
- * @min: Lower bound of checked nored
- * @max: Upper bound of checked nodes
- *
- * Return: 1 if tree is a valid BST
- *         0 otherwise
- */
-int btib_helper(const binary_tree_t *tree, int min, int max)
-{
-	if (!tree)
-		return (1);
+	if (tree != NULL)
+	{
+		current = *tree;
+		if (current == NULL)
+		{
+			new_node = binary_tree_node(current, value);
+			if (new_node == NULL)
+				return (NULL);
+			return (*tree = new_node);
+		}
+		if (value < current->n)
+		{
+			if (current->left != NULL)
+				return (bst_insert(&current->left, value));
+			new_node = binary_tree_node(current, value);
+			if (new_node == NULL)
+				return (NULL);
+			return (current->left = new_node);
+		}
+		if (value > current->n)
+		{
+			if (current->right != NULL)
+				return (bst_insert(&current->right, value));
+			new_node = binary_tree_node(current, value);
+			if (new_node == NULL)
+				return (NULL);
+			return (current->right = new_node);
+		}
+	}
 
-	if (tree->n < min || tree->n > max)
-		return (0);
-
-	return (btib_helper(tree->left, min, tree->n - 1) &&
-		btib_helper(tree->right, tree->n + 1, max));
-	/* -1 and +1 stem from "There must be no duplicate values" req */
+	return (NULL);
 }
